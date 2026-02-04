e9504f2976ff68df31b3269fbe4b2d8e15e90d75
Afaq Khan <afaqhussain.khan02@gmail.com>
2026-02-04 12:41:56 +0500

    Update save.jsx
    Removed commented-out React component code for Home.

--- save.jsx
+++ save.jsx
@@ -1,18 +1,1 @@
 
-// import React from 'react';
-// import Navbar from '../components/Navbar';
-
-// const Home = () => {
-//   return (
-//     <div>
-//       <Navbar />
-//       <div style={{ textAlign: 'center', padding: '50px' }}>
-//         <h1>Welcome to AI Knowledge Assistant</h1>
-//         <p>Upload your documents and ask questions about them!</p>
-//         <p>Go to <a href="/login">Login</a> or <a href="/signup">Sign Up</a> to start.</p>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Home;
